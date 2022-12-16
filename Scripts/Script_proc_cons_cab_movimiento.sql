4e71e5b4be2c8b0065dc83386b4fcf28158a367f
AlexanderMiranda1986 <activos86@hotmail.com>
2022-12-15 23:00:04 -0500

    Segun do Commit

--- Scripts/Script_proc_cons_cab_movimiento.sql
+++ Scripts/Script_proc_cons_cab_movimiento.sql
@@ -9,7 +9,7 @@ BEGIN
   select cab_secuencia, 
          cab_referencia as 'Referencia', 
          cab_fecha as 'Fecha', 
-         usu_codigo as 'Usuario' 
+         usu_codigo as 'Usuario_cabecera' 
    from db_inventario.inv_cab_movimiento
  where cab_fecha >= var_fecha_inicio and var_fecha_fin <= var_fecha_fin
 	and cab_tipo = var_tipo_mov;
